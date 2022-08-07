56c66fb2cc26c0aa99ab785b606f4d59c8d4cb48
Joe <joe@joedevelops.com>
2022-08-07 14:23:48 -0700

    Added missing implementations of missing interface methods.  All other methods return nil's for now.

--- githubpoxy.go
+++ githubpoxy.go
@@ -1,7 +1,8 @@
-package githubproxy
+package main
 
 import (
 	"errors"
+	"io"
 	"os"
 )
 
@@ -10,6 +11,14 @@ type GitHubProvider struct {
 
 func (ghp GitHubProvider) Login(username, password string) error {
 	return nil
+}
+
+func (ghp GitHubProvider) Policies() ([]Policy, error) {
+	return nil, nil
+}
+
+func (ghp GitHubProvider) DocumentDownload(downloadKey string) (io.ReadCloser, error) {
+	return nil, nil
 }
 
 func LoadCredentialsFromEnvironment() (username string, password string, err error) {
