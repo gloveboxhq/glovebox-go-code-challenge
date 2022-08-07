9f04df6fe1906a7c45c4310c4aaeecb4e90d0b42
Joe <joe@joedevelops.com>
2022-08-07 14:40:55 -0700

    Updated test with actual environment variables

--- githubproxy_test.go
+++ githubproxy_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -10,13 +11,18 @@ func TestGithubProxyHasLogin(t *testing.T) {
 }
 
 func TestCredentialsLoadedFromEnvironment(t *testing.T) {
+	const mock_username = "MOCK_USERNAME"
+	const mock_password = "MOCK_PASSWORD"
+	os.Setenv("username", mock_username)
+	os.Setenv("password", mock_password)
+
 	username, password, credentialsError := LoadCredentialsFromEnvironment()
 
 	if credentialsError != nil {
 		t.Fatalf("Method returned error")
 	}
 
-	if username == "" || password == "" {
-		t.Fatalf("Username or Password returned empty string")
+	if username != mock_username || password != mock_password {
+		t.Fatalf("Username or Password env vars returned unexpected value")
 	}
 }
