acf5118e38b2643c2be95264b2c8befafb9ef37f
Joe <joe@joedevelops.com>
2022-08-07 14:43:58 -0700

    Separated test files for distinction of system being tested

---
+++ utils_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCredentialsLoadedFromEnvironment(t *testing.T) {
+	const mock_username = "MOCK_USERNAME"
+	const mock_password = "MOCK_PASSWORD"
+	os.Setenv("username", mock_username)
+	os.Setenv("password", mock_password)
+
+	username, password, credentialsError := LoadCredentialsFromEnvironment()
+
+	if credentialsError != nil {
+		t.Fatalf("Method returned error")
+	}
+
+	if username != mock_username || password != mock_password {
+		t.Fatalf("Username or Password env vars returned unexpected value")
+	}
+}
