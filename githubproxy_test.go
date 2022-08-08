0a96371aea6935cf93b7fce72424589f0fdcd50d
Joe <joe@joedevelops.com>
2022-08-07 17:19:21 -0700

    Unit test to check implementation of login method using environment vars.

--- githubproxy_test.go
+++ githubproxy_test.go
@@ -8,3 +8,18 @@ func TestGithubProxyHasLogin(t *testing.T) {
 	var sut PolicyProvider = GitHubProvider{}
 	sut.Login("", "")
 }
+
+func TestGithubProxyLogsInWithEnvironmentVars(t *testing.T) {
+	var sut PolicyProvider = GitHubProvider{}
+	login, password, environmentVarErrror := LoadCredentialsFromEnvironment()
+
+	if environmentVarErrror != nil {
+		t.Fatalf("Failure loading environment variables")
+	}
+
+	loginError := sut.Login(login, password)
+
+	if loginError != nil {
+		t.Fatalf("Login failed")
+	}
+}
